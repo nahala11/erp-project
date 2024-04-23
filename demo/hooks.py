3c63081de65fcee6b4dd3b7004eac5d2cb232c18
root <nahala11>
2024-04-23 17:59:47 +0530

    initial

--- demo/hooks.py
+++ demo/hooks.py
@@ -11,7 +11,7 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/demo/css/demo.css"
+ app_include_css = "/assets/demo/css/theme.css"
 # app_include_js = "/assets/demo/js/demo.js"
 
 # include js, css files in header of web template
